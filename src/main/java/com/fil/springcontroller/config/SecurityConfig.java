48d79319dfd582aff117cdeb64fc987a443ff46f
arun.tiwari123 <arunstiwari@gmail.com>
2025-09-26 10:56:19 +0530

    oidc oauth2 implementation

--- src/main/java/com/fil/springcontroller/config/SecurityConfig.java
+++ src/main/java/com/fil/springcontroller/config/SecurityConfig.java
@@ -1,28 +1,18 @@
 package com.fil.springcontroller.config;
 
-import com.fil.springcontroller.filter.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
-//    private final JwtAuthFilter jwtAuthFilter;
 
-//    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
-//        this.jwtAuthFilter = jwtAuthFilter;
-//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
