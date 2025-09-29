7b63aeaceb8d4033c433058c39010abbba58acc5
arun.tiwari123 <arunstiwari@gmail.com>
2025-09-29 10:29:14 +0530

    Added the support for async processing

--- src/main/java/com/fil/springcontroller/exception/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.fil.springcontroller.exception;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(String message) {
-        super(message);
-    }
-}
