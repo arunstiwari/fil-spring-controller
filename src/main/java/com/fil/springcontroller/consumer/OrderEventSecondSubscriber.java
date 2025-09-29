3e13f326f4b25633a1a57b204165657838d438e1
arun.tiwari123 <arunstiwari@gmail.com>
2025-09-29 11:59:31 +0530

    Added the support for async processing

--- src/main/java/com/fil/springcontroller/consumer/OrderEventSecondSubscriber.java
+++ src/main/java/com/fil/springcontroller/consumer/OrderEventSecondSubscriber.java
@@ -2,12 +2,15 @@ package com.fil.springcontroller.consumer;
 
 import com.fil.springcontroller.entity.OrderCreatedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderEventSecondSubscriber {
+    @Async
     @EventListener
-    public  void handleOrderEvent(OrderCreatedEvent orderCreatedEvent){
+    public  void handleOrderEvent(OrderCreatedEvent orderCreatedEvent) throws InterruptedException {
+        Thread.sleep(3000);
         System.out.println("Second Subscriber is Sending Email to Customer for orderCreatedEvent = " + orderCreatedEvent);
     }
 }
