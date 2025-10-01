330878a8c333abc046a2206c7d485ed42480c61e
arun.tiwari123 <arunstiwari@gmail.com>
2025-10-01 10:07:12 +0530

    Git config server implemented

--- src/main/java/com/fil/springcontroller/service/OrderService.java
+++ src/main/java/com/fil/springcontroller/service/OrderService.java
@@ -2,21 +2,25 @@ package com.fil.springcontroller.service;
 
 import com.fil.springcontroller.producer.KafkOrderProducer;
 import com.fil.springcontroller.producer.OrderEventProducer;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
     private OrderEventProducer orderEventProducer;
     private KafkOrderProducer kafkOrderProducer;
+    private MeterRegistry meterRegistry;
 
-    public OrderService(OrderEventProducer orderEventProducer, KafkOrderProducer kafkOrderProducer) {
+    public OrderService(OrderEventProducer orderEventProducer, KafkOrderProducer kafkOrderProducer, MeterRegistry meterRegistry) {
         this.orderEventProducer = orderEventProducer;
         this.kafkOrderProducer = kafkOrderProducer;
+        this.meterRegistry = meterRegistry;
     }
 
     public void createOrder(String orderId, double amount) {
 
         orderEventProducer.publish(orderId, amount);
         kafkOrderProducer.publish(orderId, amount);
+        meterRegistry.counter("orders.created").increment();
     }
 }
